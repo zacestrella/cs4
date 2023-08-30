e307c025311e0de1144680160e7b30d984acf0a1
PHOTON <PHOTON@172.22.16.67>
2023-08-30 16:03:59 +0800

    First Java Project

---
+++ src/cs4photonestrella/CS4PhotonEstrella.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4photonestrella;
+
+/**
+ *
+ * @author PHOTON
+ */
+public class CS4PhotonEstrella {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String greeting = "Halu";
+        System.out.println(greeting + " dude");
+    }
+    
+}
